ffdbef2a4b418c4e7ee3590c0ca1295f2f3d1f95
Anastasia343 <bol.anastasia3745@gmail.com>
2022-10-22 10:39:44 +0500

    ws

---
+++ chuc_coursework/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace chuc_coursework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.buttonLogin.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.buttonLogin.Location = new System.Drawing.Point(334, 134);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(112, 33);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Войти";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.button1.Location = new System.Drawing.Point(629, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(290, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(290, 63);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::chuc_coursework.Properties.Resources._862c916898eaac1c162ebb6e62de5e81;
+            this.ClientSize = new System.Drawing.Size(753, 374);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ chuc_coursework/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace chuc_coursework
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string sha256(string randomString)
+        {
+            //Тут происходит криптографическая магия. Смысл данного метода заключается в том, что строка залетает в метод
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+        
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string connStr = "server = chuc.caseum.ru;port = 33333;user = st_2_20_4;database=is_2_20_st4_KURS;password=65655604;";
+            MySqlConnection conn;
+            conn = new MySqlConnection(connStr);
+            //string login = LoginUser.Text;
+            //string password  = PasswordUser.Text;
+
+            //MySqlCommand command = new MySqlCommand("SELECT * FROM `user`WHERE`login = @LU AND `password = `@PU ` ");
+
+            //command.Parameters.Add("@LU", MySqlDbType.VarChar).Value = LoginUser;
+            //command.Parameters.Add("@PU", MySqlDbType.VarChar).Value = PasswordUser;
+
+            //adapter.SelectCommand = command;
+
+            //Запрос в БД на предмет того, если ли строка с подходящим логином и паролем
+            string sql = "SELECT * FROM user WHERE login = @un and password= @up";
+            //Открытие соединения
+            conn.Open();
+            //Объявляем таблицу
+            DataTable table = new DataTable();
+            //Объявляем адаптер
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            //Объявляем команду
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            //Определяем параметры
+            command.Parameters.Add("@un", MySqlDbType.VarChar, 25);
+            command.Parameters.Add("@up", MySqlDbType.VarChar, 25);
+            //Присваиваем параметрам значение
+            command.Parameters["@un"].Value = textBox1.Text;
+            command.Parameters["@up"].Value = sha256(textBox2.Text);
+            //Заносим команду в адаптер
+            adapter.SelectCommand = command;
+            //Заполняем таблицу
+            adapter.Fill(table);
+            //Закрываем соединение
+            conn.Close();
+            //Если вернулась больше 0 строк, значит такой пользователь существует
+            if (table.Rows.Count > 0)
+            {
+                //Присваеваем глобальный признак авторизации
+                Auth.auth = true;
+                //Достаем данные пользователя в случае успеха
+                //GetUserInfo(textBox1.Text);
+                //Закрываем форму
+                this.Hide();
+                store gg = new store();
+                gg.Show();
+            }
+            else
+            {
+                //Отобразить сообщение о том, что авторизаия неуспешна
+                MessageBox.Show("Неверные данные авторизации!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
